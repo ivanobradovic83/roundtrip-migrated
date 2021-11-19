d965c5fc776c067c176b24ca90ffcea00d6c1cb3
Ivan Obradovic <ivanobradovic83@gmail.com>
2021-11-19 14:02:02 +0100

    get folder metadata and filter executed sequentially not in parallel, with hope to reduce number of requests

--- app/service/authormapper/mapper/AuthorFolderMapper.scala
+++ app/service/authormapper/mapper/AuthorFolderMapper.scala
@@ -21,9 +21,9 @@ class AuthorFolderMapper @Inject()(nodeApi: NodeApi) {
   private lazy val log = Logger(getClass)
   private implicit val authorFolderReads: Reads[AuthorFolder] = Json.reads[AuthorFolder]
 
-  type AuthorFolderMeta = (AuthorFolder, (String, String, String))
+  type FolderMetadata = (String, String, String)
 
-  type MappingFilter = (Author, AuthorFolderMeta) => Boolean
+  type MappingFilter = (Author, FolderMetadata) => Boolean
 
   def map(author: Author): Future[(Author, Option[AuthorFolder])] = {
     log.info(s"Mapping $author ...")
@@ -33,37 +33,60 @@ class AuthorFolderMapper @Inject()(nodeApi: NodeApi) {
   private def mapAuthorToFolder(author: Author, familyName: String, givenName: String): Future[Option[AuthorFolder]] = {
     getAuthorFoldersByMetadata(familyName, givenName)
       .flatMap {
-        case folders if notEmpty(givenName) && folders.length == 1 => authorFolderFound(author, folders.head)
+        case folders if notEmpty(givenName) && folders.length == 1 => folderFound(author, folders.head)
         case folders if folders.nonEmpty                           => mapAuthorToFolderByFilteringFoldersMetadata(folders, author)
         case _ if notEmpty(givenName)                              => mapAuthorToFolder(author, familyName, null)
-        case _                                                     => authorFolderNotFound(author)
+        case _                                                     => folderNotFound(author)
       }
   }
 
-  private def authorFolderFound(author: Author, folder: AuthorFolder) = {
-    log.info(s"Found mapping folder $folder for $author")
-    Future.successful(Option(folder))
+  private def mapAuthorToFolderByFilteringFoldersMetadata(folders: Seq[AuthorFolder], author: Author): Future[Option[AuthorFolder]] = {
+    log.info(s"Trying to map $author by folders metadata $folders ...")
+    getAuthorFolderMetadata(folders.head.id)
+      .flatMap { folderMetadata =>
+        val filters = Seq[MappingFilter](filterByFamilyGivenNameInitials, filterByFamilyGivenName, filterByFamilyNameInitials)
+        applyFilters(author, folders.head, folderMetadata, filters) match {
+          case Some(mappedFolder)        => Future.successful(Option(mappedFolder))
+          case None if folders.size == 1 => Future.successful(Option.empty)
+          case None                      => mapAuthorToFolderByFilteringFoldersMetadata(folders.tail, author)
+        }
+      }
   }
 
-  private def authorFolderNotFound(author: Author): Future[Option[AuthorFolder]] = {
-    log.info(s"No mapping folder for $author")
-    Future.successful(Option.empty)
+  @tailrec
+  private def applyFilters(author: Author,
+                           folder: AuthorFolder,
+                           folderMetadata: FolderMetadata,
+                           filters: Seq[MappingFilter]): Option[AuthorFolder] = {
+    filters.head.apply(author, folderMetadata) match {
+      case true                       => Option(folder)
+      case false if filters.size == 1 => Option.empty
+      case _                          => applyFilters(author, folder, folderMetadata, filters.tail)
+    }
   }
 
-  private def mappingError(author: Author, filteredAuthorsFolderMetadata: Seq[AuthorFolderMeta]): Future[Option[AuthorFolder]] = {
-    log.error(s"Too many folders for $author: $filteredAuthorsFolderMetadata")
-    Future.successful(Option.empty)
+  private def filterByFamilyGivenNameInitials(author: Author, folderMetadata: FolderMetadata) =
+    compare(folderMetadata._1, author.familyName) && compare(folderMetadata._2, author.givenName) && compare(folderMetadata._3, author.initials)
+
+  private def filterByFamilyGivenName(author: Author, folderMetadata: FolderMetadata) =
+    compare(folderMetadata._1, author.familyName) && compare(folderMetadata._2, author.givenName)
+
+  private def filterByFamilyNameInitials(author: Author, folderMetadata: FolderMetadata) =
+    compare(folderMetadata._1, author.familyName) && compare(folderMetadata._3, author.initials)
+
+  private def compare(value1: String, value2: String) = {
+    if (notEmpty(value1) && notEmpty(value2)) value1.replace("-", " ").equalsIgnoreCase(value2.replace("-", " "))
+    else value1 == value2
   }
 
-  private def mapAuthorToFolderByFilteringFoldersMetadata(authorFolders: Seq[AuthorFolder], author: Author): Future[Option[AuthorFolder]] = {
-    log.info(s"Trying to map $author by folders metadata $authorFolders ...")
-    Future
-      .sequence(authorFolders.map(folder => getAuthorFolderMetadata(folder.id)))
-      .flatMap(foldersMetadata => {
-        val authorsFolderMetadata = authorFolders zip foldersMetadata
-        applyFilters(author, authorsFolderMetadata, Seq(filterByFamilyGivenNameInitials, filterByFamilyGivenName, filterByFamilyNameInitials))
-      })
-  }
+  private def getAuthorFoldersByMetadata(familyName: String, givenName: String): Future[Seq[AuthorFolder]] =
+    nodeApi.getNodesByFamilyGivenNameInitials(familyName, givenName, null).map(responseToAuthorFolders)
+
+  private def responseToAuthorFolders(resp: JsValue) =
+    resp.as[Seq[JsValue]].filter(isAuthorFolder).map(Json.fromJson[AuthorFolder](_).get)
+
+  private def isAuthorFolder(node: JsValue) =
+    (node \ "nodeType").as[String] == "folder" && (node \ "documentTypePath").as[String] == "/api/documenttypes/auteursbeschrijvingen"
 
   private def getAuthorFolderMetadata(folderId: Int): Future[(String, String, String)] =
     nodeApi.getNodeMetadata(folderId).map(transformToFamilyGivenNameAndInitials)
@@ -83,46 +106,14 @@ class AuthorFolderMapper @Inject()(nodeApi: NodeApi) {
     else ""
   }
 
-  @tailrec
-  private def applyFilters(author: Author,
-                           authorsFolderMetadata: Seq[AuthorFolderMeta],
-                           filters: Seq[MappingFilter]): Future[Option[AuthorFolder]] = {
-    authorsFolderMetadata.filter(filters.head.apply(author, _)) match {
-      case filteredData if filteredData.size > 1  => mappingError(author, filteredData)
-      case filteredData if filteredData.size == 1 => authorFolderFound(author, filteredData.head._1)
-      case _ if filters.length == 1               => authorFolderNotFound(author)
-      case _                                      => applyFilters(author, authorsFolderMetadata, filters.tail)
-    }
+  private def folderFound(author: Author, folder: AuthorFolder) = {
+    log.info(s"Found mapping folder $folder for $author")
+    Future.successful(Option(folder))
   }
 
-  private def filterByFamilyGivenNameInitials(author: Author, authorFolderMeta: AuthorFolderMeta) =
-    compare(authorFolderMeta._2._1, author.familyName) && compare(authorFolderMeta._2._2, author.givenName) && compare(authorFolderMeta._2._3,
-                                                                                                                       author.initials)
-
-  private def filterByFamilyGivenName(author: Author, authorFolderMeta: AuthorFolderMeta) =
-    compare(authorFolderMeta._2._1, author.familyName) && compare(authorFolderMeta._2._2, author.givenName)
-
-  private def filterByFamilyNameInitials(author: Author, authorFolderMeta: AuthorFolderMeta) =
-    compare(authorFolderMeta._2._1, author.familyName) && compare(authorFolderMeta._2._3, author.initials)
-
-  private def compare(value1: String, value2: String) = {
-    if (notEmpty(value1) && notEmpty(value2)) value1.replace("-", " ").equalsIgnoreCase(value2.replace("-", " "))
-    else value1 == value2
+  private def folderNotFound(author: Author) = {
+    log.info(s"No mapping folder for $author")
+    Future.successful(Option.empty)
   }
-
-  private def getAuthorFoldersByMetadata(familyName: String, givenName: String): Future[Seq[AuthorFolder]] =
-    nodeApi
-      .getNodesByFamilyGivenNameInitials(familyName, givenName, null)
-      .map(responseToAuthorFolders)
-
-  private def responseToAuthorFolders(resp: JsValue) =
-    resp
-      .as[Seq[JsValue]]
-      .filter(isAuthorFolder)
-      .map(Json.fromJson[AuthorFolder](_).get)
-
-  private def isAuthorFolder(node: JsValue) =
-    (node \ "nodeType").as[String] == "folder" &&
-      (node \ "documentTypePath").as[String] == "/api/documenttypes/auteursbeschrijvingen"
 
 }
